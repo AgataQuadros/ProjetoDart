c25a7a32c22ed30496a4b28fdc0fa804da3db208
AgataQuadros <quadrosagata@gmail>
2025-09-30 21:36:02 -0300

    Operadores Aritméticos

--- aula001_estudo_de_variaveis/300925_operadores_aritimeticos.dart
+++ aula001_estudo_de_variaveis/300925_operadores_aritimeticos.dart
@@ -1,18 +1,23 @@
 import 'dart:io';
 
 void main() {
-
   stdout.write("Entre com valo de A: ");
-  int a = int.parse(stdin.readLineSync()!);
+  int a = int.parse(stdin.readLineSync()!); // fazendo casting
 
   stdout.write("Entre com valor de B: ");
-  int b = int.parse(stdin.readLineSync()!);
+  int b = int.parse(stdin.readLineSync()!); // fazendo casting
 
+  // Operador de soma
   int soma = a + b;
+  // Operador de subtração
   int subtracao = a - b;
+  // Operador de produto
   int produto = a * b;
+  // Operador de divisão
   double divisao = a / b;
+  // Operador de divisão inteira
   int divInteira = a ~/ b;
+  // Operadkr de resto da divisão
   int restoDivisao = a % b;
 
   print("-" * 50);
@@ -23,5 +28,4 @@ void main() {
   print("A divisão inteira de $a ~/ $b = $divInteira");
   print("O resto da divisão de $a % $b = $restoDivisao");
   print("-" * 50);
-  
 }
