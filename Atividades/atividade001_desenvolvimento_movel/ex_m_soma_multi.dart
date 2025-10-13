2eb9c06933723ac63af2604779302be03f0843ce
AgataQuadros <quadrosagata@gmail>
2025-10-12 23:31:50 -0300

    Exercicio M - Somar ou Multiplicar

--- Atividades/atividade001_desenvolvimento_movel/ex_m_soma_multi.dart
+++ Atividades/atividade001_desenvolvimento_movel/ex_m_soma_multi.dart
@@ -1,23 +1,49 @@
 import 'dart:io';
 
-void main() {
-  stdout.write("Digite um número: ");
-  var numero1 = int.parse(
-    stdin.readLineSync()!,
-  ); // entrada do 1º numero aleatorio
-  stdout.write("Digite um número: ");
-  var numero2 = int.parse(
-    stdin.readLineSync()!,
-  ); // entrada do 2º numero aleatorio
+// função para ler e validar um número decimal
+int lerNota(String mensagem) {
+  int? numero; // define a variavel "numero"
 
-  int resposta = 0;
-  if (numero1 == numero2) {
-    resposta = numero1 + numero2;
-  }else {
-    resposta = numero1 * numero2;
+  while (numero == null) {
+    // verifica se a variavel é null dentro de um loop
+    stdout.write(mensagem);
+    String entrada = stdin.readLineSync()!;
+    // entrada onde o usuario vai digitar o numero
+
+    entrada = entrada.replaceAll(',', '.');
+    // aceita tanto ponto quanto virgula para separar os numeros
+    // iso torna mais facil de guiar o usuario
+    numero = int.tryParse(entrada);
+    // tenta converter a entrada para numero
+
+    if (numero == null) {
+      // se não conseguir retorna a seguinte mensagem
+      print("Valor inválido!! Digite um número (ex: 7.5 ou 8.0)\n");
+    }
   }
 
-  print("~" * 20);
-  print(resposta);
-  print("~" * 20);
+  return numero;
+}
+
+void main() {
+  // usa a função para ler e validar as notas
+  var numero1 = lerNota("Digite a 1ª nota: ");
+  var numero2 = lerNota("Digite a 2ª nota: ");
+
+  int resposta = 0; // define a variave resposta
+
+  // validação para saber qual conta fazer
+  if (numero1 == numero2) {
+    resposta = numero1 + numero2;
+    print("~" * 20);
+    print("o resultado de $numero1 + $numero2 é");
+    print(resposta);
+    print("~" * 20);
+  } else {
+    resposta = numero1 * numero2;
+    print("~" * 20);
+    print("o resultado de $numero1 X $numero2 é");
+    print(resposta);
+    print("~" * 20);
+  }
 }
