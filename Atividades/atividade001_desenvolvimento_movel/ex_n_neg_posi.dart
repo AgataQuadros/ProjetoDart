55189f46c62126350ea11c8943a291b741cc2abf
AgataQuadros <quadrosagata@gmail>
2025-10-13 00:04:35 -0300

    Exercicio N - Positivo ou negativo

--- Atividades/atividade001_desenvolvimento_movel/ex_n_neg_posi.dart
+++ Atividades/atividade001_desenvolvimento_movel/ex_n_neg_posi.dart
@@ -2,18 +2,31 @@ import 'dart:io';
 
 void main() {
   stdout.write("Digite um número: ");
-  var numero = int.parse(
-    stdin.readLineSync()!,
-  ); // entrada de um numero aleatorio
+  String entrada = stdin.readLineSync()!; // entrada de um numero aleatorio
 
-  int resposta = 0;
-  if (numero <= 0) {
-    resposta = numero * 3;
-  }else {
-    resposta = numero * 2;
+  int? numero = int.tryParse(entrada); // tenta a entarda converter pra número
+
+  if (numero == null) {
+    // valida se a converção resultou em numero ou não
+    // se não retorna null e retorna a resposta de invalidez
+    print("Isso não é um número válido!");
+  } else {
+    // se sim continua o sistema normalmente
+    int resposta = 0; // define a variave resposta
+
+    // validação para saber qual conta fazer
+    if (numero <= 0) {
+      resposta = numero * 3;
+      print("~" * 20);
+      print("o triplo de $numero é");
+      print(resposta);
+      print("~" * 20);
+    } else {
+      resposta = numero * 2;
+      print("~" * 20);
+      print("o dobro se $numero é");
+      print(resposta);
+      print("~" * 20);
+    }
   }
-
-  print("~" * 20);
-  print(resposta);
-  print("~" * 20);
 }
