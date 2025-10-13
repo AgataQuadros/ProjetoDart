ea34e69c527f9f0229b1b3fabc33044f3dae4e2b
AgataQuadros <quadrosagata@gmail>
2025-10-12 22:48:19 -0300

    Exercicio G - Dobro e Triplo

--- Atividades/atividade001_desenvolvimento_movel/ex_g_dob_tri.dart
+++ Atividades/atividade001_desenvolvimento_movel/ex_g_dob_tri.dart
@@ -2,15 +2,22 @@ import 'dart:io';
 
 void main() {
   stdout.write("Digite um número: ");
-  var numero = int.parse(
-    stdin.readLineSync()!,
-  ); // entrada de um numero aleatorio
+  String entrada = stdin.readLineSync()!; // entrada de um numero aleatorio
 
-  int dobro = numero * 2;
-  int triplo = numero * 3;
+  int? numero = int.tryParse(entrada); // tenta a entarda converter pra número
 
-  print("~" * 20);
-  print("O dobro de $numero é $dobro ");
-  print("Seu triplo é $triplo");
-  print("~" * 20);
+  if (numero == null) {
+    // valida se a converção resultou em numero ou não
+    // se não retorna null e retorna a resposta de invalidez
+    print("Isso não é um número válido!");
+  } else {
+    // se sim continua o sistema normalmente
+    int dobro = numero * 2; // retorna o dobro
+    int triplo = numero * 3; // retorna o triplo
+
+    print("~" * 20);
+    print("O dobro de $numero é $dobro ");
+    print("Seu triplo é $triplo");
+    print("~" * 20);
+  }
 }
