d66349199f757a481dc99261ba46e2d99576fcc3
AgataQuadros <quadrosagata@gmail>
2025-10-27 20:59:28 -0300

    Esercício C - Soma de Multiplos

--- Atividades/atividade002_desenvolvimento_movel/ex_c_soma_multp.dart
+++ Atividades/atividade002_desenvolvimento_movel/ex_c_soma_multp.dart
@@ -1,12 +1,13 @@
-/**Faça um programa que leia um número inteiro 
- * positivo e calcule seu fatorial usando um loop. 
+/** Faça um programa que receba um número inteiro 
+ * N e calcule a soma de todos os 
+ * múltiplos de 3 ou 5 menores que N 
 */
 
 import 'dart:io';
 
-void main(){
-  while(true){
-    stdout.write("Digite o valor do dividendo: ");
+void main() {
+  while (true) {
+    stdout.write("Digite o valor do inicio: ");
     String entrada = stdin.readLineSync()!; // entrada do 1º numero
     int? numero = int.tryParse(
       entrada.replaceAll(" ", ""),
@@ -19,6 +20,34 @@ void main(){
       print("~°" * 20);
       continue; // volta para o início do loop
     }
+
+    String multiTres = "";
+    String multiCinco = "";
+    int somaTres = 0;
+    int somaCinco = 0;
+
+    for (int i = numero; i > 0; i--) {
+      if (i % 3 == 0) {
+        multiTres += "$i, ";
+        somaTres += i;
+      } else if (i % 5 == 0) {
+        multiCinco += "$i, ";
+        somaCinco += i;
+      } else {
+        continue;
+      }
+    }
+
+    print(" ");
+    print("Os multiplos de 3 do $numero são:");
+    print(multiTres);
+    print("E sua soma é $somaTres");
+    print("~" * 20);
+    print(" ");
+    print("Os multiplos de 5 do $numero são:");
+    print(multiCinco);
+    print("E sua soma é $somaCinco");
+    print("~" * 20);
 
     print(" ");
     print("~°" * 20);
