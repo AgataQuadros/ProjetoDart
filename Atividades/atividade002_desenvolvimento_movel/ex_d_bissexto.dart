d9387c2c38a183fa74ea47cd7a6d63fea7a76ca0
AgataQuadros <quadrosagata@gmail>
2025-10-27 21:13:01 -0300

    Exercício D - Ano Bixesto

--- Atividades/atividade002_desenvolvimento_movel/ex_d_bissexto.dart
+++ Atividades/atividade002_desenvolvimento_movel/ex_d_bissexto.dart
@@ -4,20 +4,34 @@
 
 import 'dart:io';
 
-void main(){
-  while(true){
-    stdout.write("Digite o valor do dividendo: ");
+void main() {
+  while (true) {
+    stdout.write("Digite apenas o ano: ");
     String entrada = stdin.readLineSync()!; // entrada do 1º numero
-    int? numero = int.tryParse(
+    int? ano = int.tryParse(
       entrada.replaceAll(" ", ""),
     ); // retira qulquer espaço na entrada
     print("~" * 20);
     // valida a entrada
-    if (numero == null || numero.isNegative) {
+    if (ano == null || ano.isNegative) {
       print("");
       print("Valor inválida! Digite um número inteiro e positivo");
       print("~°" * 20);
       continue; // volta para o início do loop
+    }
+
+    if (ano % 400 == 0) {
+      print(" ");
+      print("O ano de $ano é bissexto!");
+      print("~°" * 20);
+    } else if (ano % 4 == 0 && ano % 100 != 0) {
+      print(" ");
+      print("O ano de $ano é bissexto!");
+      print("~°" * 20);
+    } else {
+      print(" ");
+      print("O ano de $ano não é bissexto!");
+      print("~°" * 20);
     }
 
     print(" ");
