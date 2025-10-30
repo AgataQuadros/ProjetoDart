396c924d1d257e49f37ca98c58662d189b374b86
AgataQuadros <quadrosagata@gmail>
2025-10-29 21:28:34 -0300

    Varrendo listas com for

---
+++ aula010_listas/291025_lista_for.dart
@@ -0,0 +1,12 @@
+void main(){
+  List<String> vogais = ['a','e','i','o','u'];
+
+  print("~°" * 20);
+  print("Lista de vogais");
+
+  for (var i = 0; i < vogais.length; i++){
+    print(vogais[i]);
+  }
+
+  print("~°" * 20);
+}
