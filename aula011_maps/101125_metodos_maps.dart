c93d42c6da07f3ce521abb4be6c12bf2a1f51af4
AgataQuadros <quadrosagata@gmail>
2025-11-10 21:40:08 -0300

    QWQ

---
+++ aula011_maps/101125_metodos_maps.dart
@@ -0,0 +1,72 @@
+void main() {
+  Map<String, int> mapLength = {"a": 1, "b": 2, "c": 3};
+  print("O amanho desse mapa é: ${mapLength.length}");
+
+  dynamic mapTesteVazio = {};
+  dynamic mapTesteCheio = {"a": 1};
+
+  print("Map 'mapTesteVazio' esta vazio? ${mapTesteVazio.isEmpty}");
+  print("Map 'mapTesteVazio' esta cheio? ${mapTesteVazio.isNotEmpty}");
+
+  print("Map 'mapTesteCheio' esta vazio? ${mapTesteCheio.isEmpty}");
+  print("Map 'mapTesteCheio' esta cheio? ${mapTesteCheio.isNotEmpty}");
+  print("-" * 70);
+
+
+  Map<String, int> mapChaveValor = {"a": 1, "b": 2, "c": 3};
+  print("O amanho desse mapa é: ${mapLength.length}");
+
+  dynamic mapTesteVazio = {};
+  dynamic mapTesteCheio = {"a": 1};
+
+  print("Map 'mapTesteVazio' esta vazio? ${mapTesteVazio.isEmpty}");
+  print("Map 'mapTesteVazio' esta cheio? ${mapTesteVazio.isNotEmpty}");
+
+  print("Map 'mapTesteCheio' esta vazio? ${mapTesteCheio.isEmpty}");
+  print("Map 'mapTesteCheio' esta cheio? ${mapTesteCheio.isNotEmpty}");
+  print("-" * 70);
+
+
+  Map<String, int> mapChaveValor = {"a": 1, "b": 2, "c": 3};
+  print("O amanho desse mapa é: ${mapLength.length}");
+
+  dynamic mapTesteVazio = {};
+  dynamic mapTesteCheio = {"a": 1};
+
+  print("Map 'mapTesteVazio' esta vazio? ${mapTesteVazio.isEmpty}");
+  print("Map 'mapTesteVazio' esta cheio? ${mapTesteVazio.isNotEmpty}");
+
+  print("Map 'mapTesteCheio' esta vazio? ${mapTesteCheio.isEmpty}");
+  print("Map 'mapTesteCheio' esta cheio? ${mapTesteCheio.isNotEmpty}");
+  print("-" * 70);
+
+
+  Map<String, int> mapLength = {"a": 1, "b": 2, "c": 3};
+  print("O amanho desse mapa é: ${mapLength.length}");
+
+  dynamic mapTesteVazio = {};
+  dynamic mapTesteCheio = {"a": 1};
+
+  print("Map 'mapTesteVazio' esta vazio? ${mapTesteVazio.isEmpty}");
+  print("Map 'mapTesteVazio' esta cheio? ${mapTesteVazio.isNotEmpty}");
+
+  print("Map 'mapTesteCheio' esta vazio? ${mapTesteCheio.isEmpty}");
+  print("Map 'mapTesteCheio' esta cheio? ${mapTesteCheio.isNotEmpty}");
+  print("-" * 70);
+
+
+  Map<String, int> mapLength = {"a": 1, "b": 2, "c": 3};
+  print("O amanho desse mapa é: ${mapLength.length}");
+
+  dynamic mapTesteVazio = {};
+  dynamic mapTesteCheio = {"a": 1};
+
+  print("Map 'mapTesteVazio' esta vazio? ${mapTesteVazio.isEmpty}");
+  print("Map 'mapTesteVazio' esta cheio? ${mapTesteVazio.isNotEmpty}");
+
+  print("Map 'mapTesteCheio' esta vazio? ${mapTesteCheio.isEmpty}");
+  print("Map 'mapTesteCheio' esta cheio? ${mapTesteCheio.isNotEmpty}");
+  print("-" * 70);
+
+
+}
