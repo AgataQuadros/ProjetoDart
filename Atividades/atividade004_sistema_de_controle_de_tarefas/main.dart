f4cbf7ccf86d0d15f81ba13bdddf2ed4f44f9d28
AgataQuadros <quadrosagata@gmail>
2025-11-13 18:49:36 -0300

    Quase finalizado !!!!!!!!!

--- Atividades/atividade004_sistema_de_controle_de_tarefas/main.dart
+++ Atividades/atividade004_sistema_de_controle_de_tarefas/main.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main() {
   List<Map<String, dynamic>> tarefas = [];
   int proximoId = 1;
-  
+
   while (true) {
     print(" ");
     print("~°" * 60);
@@ -19,7 +19,7 @@ void main() {
     stdout.write("Entre com a opção de acordo com o menu: ");
     // úsuario escolhe uma das opções de ação para inicias o sistema
     String? entrada = stdin.readLineSync();
-    int? opcao = int.tryParse(entrada ?? '');
+    int? opcao = int.tryParse(entrada ?? "");
     if (opcao == null || opcao <= 0) {
       print("Entrada invalida! Digite Algo!");
       continue;
@@ -56,7 +56,7 @@ void main() {
       stdout.write("Entre com o status da tarefa: ");
       String? entradaStatus = stdin.readLineSync();
       // o úsuario adiciona um status, no terminal vai aparecer a palavra
-      int? status = int.tryParse(entradaStatus ?? '');
+      int? status = int.tryParse(entradaStatus ?? "");
       if (status == null || status <= 0) {
         print("Entrada invalida! Digite Algo!");
         continue;
@@ -77,12 +77,120 @@ void main() {
       tarefas.add({
         "id": proximoId,
         "titulo": tarefa,
-        "descricao": descricao,
+        "descrição": descricao,
         "status": statusTexto,
       });
       proximoId++; // incrementa o ID automaticamente
     } else if (opcao == 2) {
+      print(".°" * 30);
       tarefas.forEach((tarefa) => print(tarefa));
+      print(".°" * 30);
+    } else if (opcao == 3) {
+      stdout.write("Entre com o id da tarefa:");
+      String? entradaId = stdin.readLineSync();
+      int? id = int.tryParse(entradaId ?? "");
+      if (id == null || id <= 0) {
+        print("Entrada invalida! Digite Algo!");
+        continue;
+      }
+      var porTarefa = tarefas.firstWhere(
+        (t) => t['id'] == id,
+        orElse: () => {},
+      );
+      if (porTarefa.isEmpty) {
+        print("Essa tarefa não existe");
+        continue;
+      }
+
+      print(".°" * 30);
+      print("-=-=-=-=-=- ATUALIZAR -=-=-=-=-=-");
+      print("  1 - Titulo");
+      print("  2 - Descrição");
+      print("  3 - Status");
+      print(".°" * 30);
+
+      stdout.write("Entre com a opção de atualizaçaõ: ");
+      String? entradaAtualizar = stdin.readLineSync();
+      // o úsuario adiciona um status, no terminal vai aparecer a palavra
+      int? opAtt = int.tryParse(entradaAtualizar ?? "");
+      if (opAtt == null || opAtt <= 0) {
+        print("Entrada invalida! Digite Algo!");
+        continue;
+      } else if (opAtt > 3) {
+        print("Entrada invalida! Digite um Número Dentro das Opções!");
+        continue;
+      }
+
+      if (opAtt == 1) {
+        stdout.write("Entre com a tarefa: ");
+        // úsuario entra com a tarefa em si
+        String? tituloNovo = stdin.readLineSync();
+        if (tituloNovo == null || tituloNovo.trim().isEmpty) {
+          print("Entrada invalida! Digite Algo!");
+          continue;
+        }
+
+        porTarefa['titulo'] = tituloNovo;
+      } else if (opAtt == 2) {
+        stdout.write("Entre com a nova descrição: ");
+        // úsuario entra com a tarefa em si
+        String? descricaoNova = stdin.readLineSync();
+        if (descricaoNova == null || descricaoNova.trim().isEmpty) {
+          print("Entrada invalida! Digite Algo!");
+          continue;
+        }
+
+        porTarefa['descrição'] = descricaoNova;
+      } else if (opAtt == 3) {
+        print(".°" * 30);
+        print("-=-=-=-=-=- STATUS -=-=-=-=-=-");
+        print("  1 - Ativa");
+        print("  2 - Em Andamento");
+        print("  3 - Finalizada");
+        print(".°" * 30);
+
+        stdout.write("Entre com o novo status da tarefa: ");
+        String? entradaStatus = stdin.readLineSync();
+        // o úsuario adiciona um status, no terminal vai aparecer a palavra
+        int? status = int.tryParse(entradaStatus ?? "");
+        if (status == null || status <= 0) {
+          print("Entrada invalida! Digite Algo!");
+          continue;
+        } else if (status > 3) {
+          print("Entrada invalida! Digite um Número Dentro das Opções!");
+          continue;
+        }
+
+        String statusTexto = status.toString();
+        if (status == 1) {
+          statusTexto = "Ativa";
+        } else if (status == 2) {
+          statusTexto = "Em Andamento";
+        } else if (status == 3) {
+          statusTexto = "Finalizada";
+        }
+
+        porTarefa['status'] = statusTexto;
+      }
+    } else if (opcao == 4) {
+      stdout.write("Entre com o id da tarefa:");
+      String? entradaId = stdin.readLineSync();
+      int? id = int.tryParse(entradaId ?? "");
+      if (id == null || id <= 0) {
+        print("Entrada invalida! Digite Algo!");
+        continue;
+      }
+      var porTarefa = tarefas.firstWhere(
+        (t) => t['id'] == id,
+        orElse: () => {},
+      );
+      if (porTarefa.isEmpty) {
+        print("Essa tarefa não existe");
+        continue;
+      }
+
+      tarefas.removeWhere((t) => t['id'] == id);
+      print("Tarefa excluida com sucesso!");
     }
   }
 }
