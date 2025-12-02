a53b9edf68aba612f5d313932b78ea9ba5f195b6
AgataQuadros <quadrosagata@gmail>
2025-12-01 21:35:12 -0300

    IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIAaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Atividades/atividade003_Listas/atividade003_Listas_Refatorada/ex_i_email.dart
+++ Atividades/atividade003_Listas/atividade003_Listas_Refatorada/ex_i_email.dart
@@ -6,62 +6,88 @@ endereços de email, filtre e colete em uma
 nova lista apenas os emails que contêm 
 exatamente um caractere '@' e terminam com ".com".
 */
-
-void main() {
-  print(" ");
-  print("~°" * 20);
-  print("~" * 20);
+String lerString(lista) {
   while (true) {
-    int? numero;
-    while (true) {
+    print(" ");
+    print("~°" * 20);
+    stdout.write('Entre com um email básico: ');
+    String entrada = stdin.readLineSync()!;
+    print("");
+    print("~" * 20);
+
+    String tratada = entrada.trim();
+
+    if (tratada.isEmpty) {
       print(" ");
+      print('Resposta inválida!');
       print("~°" * 20);
-      stdout.write("Digite um número: ");
-      String entrada = stdin.readLineSync()!;
-      print("");
-      print("~" * 20);
-      numero = int.tryParse(entrada.replaceAll(" ", ""));
-      if (numero == null || numero.isNegative) {
-        print("Por favor digite um número válido");
-        print(" ");
-        print("~°" * 20);
-        continue;
-      } else {
-        break;
-      }
+      continue;
     }
 
-    while (true) {
+    return tratada;
+  }
+}
+
+bool desejaContinuar() {
+  while (true) {
+    print(" ");
+    stdout.write('Deseja continuar? (s/n): ');
+    String? resposta = stdin.readLineSync();
+    print("~" * 20);
+
+    if (resposta == null || resposta.isEmpty) {
       print(" ");
-      stdout.write('Deseja continuar? (s/n): ');
-      String? resposta = stdin.readLineSync();
-      print("~" * 20);
+      print('Resposta inválida!');
+      print("~°" * 20);
+      continue;
+    }
 
-      if (resposta == null || resposta.isEmpty) {
-        
-        print(" ");
-        print('Resposta inválida!');
-        print("~°" * 20);
-        continue;
-      }
+    resposta = resposta.toLowerCase();
 
-      resposta = resposta.toLowerCase();
+    if (resposta == 'n') {
+      print(" ");
+      print("Obrigado por usar! Encerrando o programa... ");
+      print("~°" * 20);
+      return false;
+    } else if (resposta == 's') {
+      return true; // Sai do laço e volta pro início da calculadora
+    } else {
+      print(" ");
+      print('Opção inválida! Digite "s" ou "n".');
+      print("~°" * 20);
+    }
+  }
+}
 
-      if (resposta == 'n') {
+void validaEmail(palavra) {
+  bool retorna = true;
+  while(retorna){
+    var filtroArroba = palavra.contains("@");
+    var filtroCom = palavra.endsWith(".com");
 
-        print(" ");
-        print("Obrigado por usar! Encerrando o programa... ");
-        print("~°" * 20);
-        return;
-      } else if (resposta == 's') {
+    if (filtroArroba == true && filtroCom == true) {
+      print(palavra);
+      print("É um email valido!");
+      print("Parabéns você esta cadastrado(a, e)!");
+      retorna = false;
+    } else {
+      print("Oh não falta algo no seu email!");
+      print("Você tem certeza que ele contem um '@' e termina com '.com'?");
+      break;
+    }
+  }
+}
 
-        break; // Sai do laço e volta pro início da calculadora
-      } else {
+void main() {
+  while (true) {
+    List<String> lista = [];
+    var palavra = lerString(lista);
 
-        print(" ");
-        print('Opção inválida! Digite "s" ou "n".');
-        print("~°" * 20);
-      }
+    validaEmail(palavra);
+
+    if (!desejaContinuar()) {
+      print('Fim do programa!');
+      return;
     }
   }
 }
