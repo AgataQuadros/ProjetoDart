39dec39372c10c23f1918c4eea9d2b39e1fd2ebe
AgataQuadros <quadrosagata@gmail>
2025-12-03 21:32:49 -0300

    Ativida 5 - Orientação a objeto

---
+++ Atividades/atividade005_orientado_a_objeto/titulos.dart
@@ -0,0 +1,38 @@
+/*
+1 - Faça um programa que peça 2 valores. Calcule e imprima  a soma, o produto, a subtração, a divisão, o resto da divisão e a divisão inteira.
+
+2 - Faça um programa que peça um ano qualquer. O programa deverá calcular e imprimir a idade.
+
+3 - Faça um programa que peça 4 notas com  entrada de dados. O programa deverá calcular a média das notas digitadas.
+
+4 - Faça um programa que receba e divida 2 números. A saída da divisão precisará ser formatada com 4 casas decimais.
+
+5 - Faça um programa que recebe um número inteiro e mostre o sucessor e antecessor.
+
+6 - Faça um programa que receba um número qualquer e calcule o dobro e o triplo desse número.
+
+7 - Faça um programa para converter medidas
+
+8 - Faça um programa que receba um número inteiro. O programa deverá imprimir tabuada de multiplicação deste número.
+
+9 - Faça um programa que receba um valor em reais, depois calcule quantos euros, dólares e wons dariam para comprar com esse valor.
+
+10 - Faça um programa com entrada de dados para calcular a área de 4 objetos.
+
+11 - Faça um programa para receber um número qualquer e informar na tela se é par ou ímpar. 
+
+12 - Faça um algoritmo que leia dois valores inteiros A e B. Se os valores forem iguais deverá somar os dois, caso contrário multiplique A por B. Ao final de qualquer um dos cálculos deve-se atribuir o resultado para uma variável C e mostrar seu conteúdo na tela. 
+
+13 - Faça um programa para encontrar o dobro de um número caso ele seja positivo e o seu triplo caso seja negativo, imprimindo o resultado. 
+
+14 - Faça um programa que leia uma variável e some 5 caso seja par ou some 8 caso seja ímpar, imprimir o resultado desta operação. 
+
+15 - Faça um programa que leia três valores inteiros e diferentes e mostre-os em ordem decrescente. 
+
+16 - Faça um programa para calcular o IMC COMPLETO de uma pessoa.
+
+17 - Faça um programa para conversão de temperaturas (C => F  e F ⇒ C)
+
+18 - Faça um programa para calcular juros.
+
+*/
