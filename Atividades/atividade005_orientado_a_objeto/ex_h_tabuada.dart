ae57bc445118e45369fb345783107ff30e465831
AgataQuadros <quadrosagata@gmail>
2025-12-09 21:22:18 -0300

    Atividade 005: Classes - Tabuada

---
+++ Atividades/atividade005_orientado_a_objeto/ex_h_tabuada.dart
@@ -0,0 +1,115 @@
+/* 8 - Faça um programa que receba um número inteiro. 
+O programa deverá imprimir tabuada 
+de multiplicação deste número. */
+
+import 'dart:io';
+
+// classe de validação de dados
+class Validacao {
+  // lê um inteiro (usado para escolher opções do menu)
+  int lerInt(String mensagem) {
+    int? numero; // define a variavel "numero"
+
+    while (numero == null) {
+      // repete até o usuário digitar um número válido
+      // verifica se a variavel é null dentro de um loop
+      stdout.write(mensagem); // mostra a mensagem de entrada no terminal
+      String entrada = stdin.readLineSync()!;
+      // entrada onde o usuario vai digitar o numero
+
+      if (entrada.length > 4 || entrada.length <= 0) {
+        print(
+          "Por favor entre só com o ano em que você nasceu e o ano a comparar(ex: 2025-2006)",
+        );
+        print(".°" * 30);
+      } else {
+        numero = int.tryParse(entrada);
+        // tenta converter a entrada para numero
+
+        if (numero == null) {
+          // se não conseguir retorna a seguinte mensagem
+          print("Valor inválido!! Digite um número(ex:1,2,3,4,5)");
+          print("~-" * 30);
+        }
+      }
+    }
+
+    return numero; // retorna o número válido digitado
+  }
+
+  bool desejaContinuar() {
+    while (true) {
+      print(" ");
+      stdout.write(
+        'Deseja continuar? (s/n): ',
+      ); // mostra a mensagem de entrada no terminal
+      String? resposta = stdin
+          .readLineSync(); // entrada onde o usuario vai digitar a resposta
+      print("~-" * 30);
+
+      // valida se a entrada é nula e responde com erro
+      if (resposta == null || resposta.isEmpty) {
+        print(" ");
+        print('Resposta inválida!');
+        print(".°" * 30);
+        continue;
+      }
+
+      // passa a resposta para caixa baixa para facilitar a validação
+      resposta = resposta.toLowerCase();
+
+      // caso seja n(não) enviara esta mensagem
+      if (resposta == 'n') {
+        print(" ");
+        print("Obrigado por usar! Encerrando o programa... ");
+        print("~°" * 60);
+        return false;
+      }
+      // caso seja s(sim) enviara essa resposta
+      else if (resposta == 's') {
+        return true; // Sai do laço e volta pro início da calculadora
+      }
+      // se for qualquer outra coisa enviara essa mensagem de erro
+      else {
+        print(" ");
+        print('Opção inválida! Digite "s" ou "n".');
+        print(".°" * 30);
+      }
+    }
+  }
+}
+
+class Tabuada {
+  var numero;
+  Tabuada({required this.numero});
+
+  void exibir() {
+    print(" ");
+    print("~-" * 30);
+    for (int i = 1; i <= 10; i++) {
+      print("$numero X $i = ${numero * i}");
+    }
+    print("~-" * 30);
+  }
+}
+
+void main() {
+  while (true) {
+    print(" ");
+    print("~°" * 60);
+
+    Validacao validacao = Validacao();
+
+    int numero = validacao.lerInt("Entre com um número inteir aleatorio: ");
+
+    Tabuada tabuada = Tabuada(numero: numero);
+
+    tabuada.exibir();
+
+    bool continuar = validacao.desejaContinuar();
+
+    if (!continuar) {
+      break;
+    }
+  }
+}
